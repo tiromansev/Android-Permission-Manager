5ef206a38e22006f24ece9f6866bb66d16ebf1ad
tiroman <nbvjattd1980>
2017-09-13 10:47:32 +0300

    first commit

---
+++ permissionmanager/src/main/java/com/tiromansev/permissionmanager/PermissionsManager.java
@@ -0,0 +1,372 @@
+package com.tiromansev.permissionmanager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ADD_VOICEMAIL;
+import static android.Manifest.permission.BODY_SENSORS;
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.GET_ACCOUNTS;
+import static android.Manifest.permission.PROCESS_OUTGOING_CALLS;
+import static android.Manifest.permission.READ_CALENDAR;
+import static android.Manifest.permission.READ_CALL_LOG;
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.READ_SMS;
+import static android.Manifest.permission.RECEIVE_MMS;
+import static android.Manifest.permission.RECEIVE_SMS;
+import static android.Manifest.permission.RECEIVE_WAP_PUSH;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.SEND_SMS;
+import static android.Manifest.permission.USE_SIP;
+import static android.Manifest.permission.WRITE_CALENDAR;
+import static android.Manifest.permission.WRITE_CALL_LOG;
+import static android.Manifest.permission.WRITE_CONTACTS;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class PermissionsManager {
+
+    public final static int LOCATION_REQUEST = 100;
+    public final static int WRITE_EXTERNAL_REQUEST = 101;
+    public final static int READ_CALENDAR_REQUEST = 102;
+    public final static int WRITE_CALENDAR_REQUEST = 103;
+    public final static int CAMERA_REQUEST = 104;
+    public final static int READ_CONTACTS_REQUEST = 105;
+    public final static int WRITE_CONTACTS_REQUEST = 106;
+    public final static int GET_ACCOUNTS_REQUEST = 107;
+    public final static int ACCESS_FINE_LOCATION_REQUEST = 108;
+    public final static int ACCESS_COARSE_LOCATION_REQUEST = 109;
+    public final static int RECORD_AUDIO_REQUEST = 110;
+    public final static int READ_PHONE_STATE_REQUEST = 111;
+    public final static int CALL_PHONE_REQUEST = 112;
+    public final static int READ_CALL_LOG_REQUEST = 113;
+    public final static int WRITE_CALL_LOG_REQUEST = 114;
+    public final static int ADD_VOICEMAIL_REQUEST = 115;
+    public final static int USE_SIP_REQUEST = 116;
+    public final static int PROCESS_OUTGOING_CALLS_REQUEST = 117;
+    public final static int BODY_SENSORS_REQUEST = 118;
+    public final static int SEND_SMS_REQUEST = 119;
+    public final static int RECEIVE_SMS_REQUEST = 120;
+    public final static int READ_SMS_REQUEST = 121;
+    public final static int RECEIVE_WAP_PUSH_REQUEST = 122;
+    public final static int RECEIVE_MMS_REQUEST = 123;
+    public final static int READ_EXTERNAL_STORAGE_REQUEST = 124;
+
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected;
+    private PermissionCallback permissionCallback;
+    private View snackBarParent;
+    private Activity context;
+    private static Context appContext;
+    private static SharedPreferences appPreferences;
+
+    public static void setAppContext(Context appContext) {
+        PermissionsManager.appContext = appContext;
+    }
+
+    public void attachToView(Activity context, View snackBarParent) {
+        this.context = context;
+        this.snackBarParent = snackBarParent;
+        appPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public void checkPermission(int permissionId, PermissionCallback permissionCallback) {
+        if (context == null) {
+            throw new RuntimeException(appContext.getString(R.string.message_context_is_null));
+        }
+
+        ArrayList<String> permissions = new ArrayList<>();
+        this.permissionCallback = permissionCallback;
+
+        switch (permissionId) {
+            case LOCATION_REQUEST:
+                permissions.add(ACCESS_FINE_LOCATION);
+                break;
+            case WRITE_EXTERNAL_REQUEST:
+                permissions.add(WRITE_EXTERNAL_STORAGE);
+                break;
+            case READ_CALENDAR_REQUEST:
+                permissions.add(READ_CALENDAR);
+                break;
+            case WRITE_CALENDAR_REQUEST:
+                permissions.add(WRITE_CALENDAR);
+                break;
+            case CAMERA_REQUEST:
+                permissions.add(CAMERA);
+                break;
+            case READ_CONTACTS_REQUEST:
+                permissions.add(READ_CONTACTS);
+                break;
+            case WRITE_CONTACTS_REQUEST:
+                permissions.add(WRITE_CONTACTS);
+                break;
+            case GET_ACCOUNTS_REQUEST:
+                permissions.add(GET_ACCOUNTS);
+                break;
+            case ACCESS_FINE_LOCATION_REQUEST:
+                permissions.add(ACCESS_FINE_LOCATION);
+                break;
+            case ACCESS_COARSE_LOCATION_REQUEST:
+                permissions.add(ACCESS_COARSE_LOCATION);
+                break;
+            case RECORD_AUDIO_REQUEST:
+                permissions.add(RECORD_AUDIO);
+                break;
+            case READ_PHONE_STATE_REQUEST:
+                permissions.add(READ_PHONE_STATE);
+                break;
+            case CALL_PHONE_REQUEST:
+                permissions.add(CALL_PHONE);
+                break;
+            case READ_CALL_LOG_REQUEST:
+                permissions.add(READ_CALL_LOG);
+                break;
+            case WRITE_CALL_LOG_REQUEST:
+                permissions.add(WRITE_CALL_LOG);
+                break;
+            case ADD_VOICEMAIL_REQUEST:
+                permissions.add(ADD_VOICEMAIL);
+                break;
+            case USE_SIP_REQUEST:
+                permissions.add(USE_SIP);
+                break;
+            case PROCESS_OUTGOING_CALLS_REQUEST:
+                permissions.add(PROCESS_OUTGOING_CALLS);
+                break;
+            case BODY_SENSORS_REQUEST:
+                permissions.add(BODY_SENSORS);
+                break;
+            case SEND_SMS_REQUEST:
+                permissions.add(SEND_SMS);
+                break;
+            case RECEIVE_SMS_REQUEST:
+                permissions.add(RECEIVE_SMS);
+                break;
+            case READ_SMS_REQUEST:
+                permissions.add(READ_SMS);
+                break;
+            case RECEIVE_WAP_PUSH_REQUEST:
+                permissions.add(RECEIVE_WAP_PUSH);
+                break;
+            case RECEIVE_MMS_REQUEST:
+                permissions.add(RECEIVE_MMS);
+                break;
+            case READ_EXTERNAL_STORAGE_REQUEST:
+                permissions.add(READ_EXTERNAL_STORAGE);
+                break;
+        }
+
+        permissionsToRequest = findUnAskedPermissions(permissions);
+        permissionsRejected = findRejectedPermissions(permissions);
+
+        if (permissionsToRequest.size() > 0) {//we need to ask for permissions
+            ActivityCompat.requestPermissions(context,
+                    permissionsToRequest.toArray(new String[permissionsToRequest.size()]), permissionId);
+            for (String perm : permissionsToRequest) {
+                markAsAsked(perm);
+            }
+        } else {
+            if (permissionsRejected.size() > 0) {
+                if (snackBarParent != null) {
+                    Snackbar
+                            .make(snackBarParent, String.valueOf(permissionsRejected.size()) +
+                                    " " + appContext.getString(R.string.caption_permission_previously_rejected), Snackbar.LENGTH_LONG)
+                            .setAction(appContext.getString(R.string.caption_allow_ask_again), new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    for (String perm : permissionsRejected) {
+                                        clearMarkAsAsked(perm);
+                                    }
+                                }
+                            })
+                            .show();
+                    permissionCallback.permissionRejected();
+                } else {
+                    Toast.makeText(context, appContext.getString(R.string.message_snakbar_parent_is_null), Toast.LENGTH_LONG).show();
+                }
+            } else {
+                this.permissionCallback.permissionAccepted();
+            }
+        }
+    }
+
+    private void requestPermission(String permission) {
+        if (hasPermission(permission)) {
+            this.permissionCallback.permissionAccepted();
+        } else {
+            permissionsRejected.add(permission);
+            makePostRequestSnack();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case WRITE_EXTERNAL_REQUEST:
+                requestPermission(WRITE_EXTERNAL_STORAGE);
+                break;
+            case LOCATION_REQUEST:
+                requestPermission(ACCESS_FINE_LOCATION);
+                break;
+            case READ_CALENDAR_REQUEST:
+                requestPermission(READ_CALENDAR);
+                break;
+            case WRITE_CALENDAR_REQUEST:
+                requestPermission(WRITE_CALENDAR);
+                break;
+            case CAMERA_REQUEST:
+                requestPermission(CAMERA);
+                break;
+            case READ_CONTACTS_REQUEST:
+                requestPermission(READ_CONTACTS);
+                break;
+            case WRITE_CONTACTS_REQUEST:
+                requestPermission(WRITE_CONTACTS);
+                break;
+            case GET_ACCOUNTS_REQUEST:
+                requestPermission(GET_ACCOUNTS);
+                break;
+            case ACCESS_FINE_LOCATION_REQUEST:
+                requestPermission(ACCESS_FINE_LOCATION);
+                break;
+            case ACCESS_COARSE_LOCATION_REQUEST:
+                requestPermission(ACCESS_COARSE_LOCATION);
+                break;
+            case RECORD_AUDIO_REQUEST:
+                requestPermission(RECORD_AUDIO);
+                break;
+            case READ_PHONE_STATE_REQUEST:
+                requestPermission(READ_PHONE_STATE);
+                break;
+            case CALL_PHONE_REQUEST:
+                requestPermission(CALL_PHONE);
+                break;
+            case READ_CALL_LOG_REQUEST:
+                requestPermission(READ_CALL_LOG);
+                break;
+            case WRITE_CALL_LOG_REQUEST:
+                requestPermission(WRITE_CALL_LOG);
+                break;
+            case ADD_VOICEMAIL_REQUEST:
+                requestPermission(ADD_VOICEMAIL);
+                break;
+            case USE_SIP_REQUEST:
+                requestPermission(USE_SIP);
+                break;
+            case PROCESS_OUTGOING_CALLS_REQUEST:
+                requestPermission(PROCESS_OUTGOING_CALLS);
+                break;
+            case BODY_SENSORS_REQUEST:
+                requestPermission(BODY_SENSORS);
+                break;
+            case SEND_SMS_REQUEST:
+                requestPermission(SEND_SMS);
+                break;
+            case RECEIVE_SMS_REQUEST:
+                requestPermission(RECEIVE_SMS);
+                break;
+            case READ_SMS_REQUEST:
+                requestPermission(READ_SMS);
+                break;
+            case RECEIVE_WAP_PUSH_REQUEST:
+                requestPermission(RECEIVE_WAP_PUSH);
+                break;
+            case RECEIVE_MMS_REQUEST:
+                requestPermission(RECEIVE_MMS);
+                break;
+            case READ_EXTERNAL_STORAGE_REQUEST:
+                requestPermission(READ_EXTERNAL_STORAGE);
+                break;
+        }
+    }
+
+    private void makePostRequestSnack() {
+        if (snackBarParent != null) {
+            Snackbar
+                    .make(snackBarParent, String.valueOf(permissionsRejected.size()) + " " +
+                            appContext.getString(R.string.caption_permission_rejected),
+                            Snackbar.LENGTH_LONG)
+                    .setAction(appContext.getString(R.string.caption_allow_ask_again), new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            for (String perm : permissionsRejected) {
+                                clearMarkAsAsked(perm);
+                            }
+                        }
+                    })
+                    .show();
+            permissionCallback.permissionRejected();
+        } else {
+            Toast.makeText(context, appContext.getString(R.string.message_snakbar_parent_is_null), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean hasPermission(String permission) {
+        return !canMakeSmores() ||
+                (ActivityCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private boolean shouldWeAsk(String permission) {
+        return appPreferences.getBoolean(permission, true);
+    }
+
+    private void markAsAsked(String permission) {
+        SharedPreferences.Editor edit = appPreferences.edit();
+        edit.putBoolean(permission, false);
+        edit.apply();
+    }
+
+    private void clearMarkAsAsked(String permission) {
+        SharedPreferences.Editor edit = appPreferences.edit();
+        edit.putBoolean(permission, true);
+        edit.apply();
+    }
+
+    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm) && shouldWeAsk(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private ArrayList<String> findRejectedPermissions(ArrayList<String> wanted) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm) && !shouldWeAsk(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean canMakeSmores() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    public interface PermissionCallback {
+        void permissionAccepted();
+        void permissionRejected();
+    }
+
+}
